dc939651a16c839f078a5ac2f9e5091cd1e67995
EvanDenerley <EvanDenerley@f764933d-b079-6ae2-75ea-41378b6658d9>
2012-02-22 01:14:26 +0000

    First Upload of the current Bike GUI with Accel/Orientation/Slider

---
+++ Android/EVcode/HelloTabWidget/src/com/tab/SettingsActivity.java
@@ -0,0 +1,11 @@
+package com.tab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_settings);
+    }
+}
